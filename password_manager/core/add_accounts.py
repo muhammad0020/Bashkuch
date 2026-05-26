c56685d9307253adda36e406504e045dcac1d9ee
Muhammad Hosein Yeganeh <muhammad.hosein.yeganeh@gmail.com>
2026-05-26 19:44:50 +0330

    docs: improve docstrings and comments

--- password_manager/core/add_accounts.py
+++ password_manager/core/add_accounts.py
@@ -1,7 +1,7 @@
 """
 Adds new accounts with Fernet-encrypted credentials.
 
-"Collects service name, username, and password from the user and encrypts them using Fernet"
+Collects service name, username, and password from the user and encrypts them using Fernet.
 
 Stores encrypted data in JSON file.
 """
@@ -15,53 +15,48 @@ from ..utils import save_data
 def create_accounts(accounts: list, deleted_accounts: list, unique_keys: set):
     """
     Prompts user for service name, username, and password.
-    Encrypts and saves to vault. Prevents duplicates by checking username uniqueness.
+    Encrypts and saves to vault. Prevents duplicates by checking service name and username uniqueness.
 
     Parameters:
-        - accounts_list (list): Saved accounts in vault.
-        - recycle_bin_data (list): Deleted accounts in deleted_accounts list.
-        - unique_keys (set): Set of unique service names and usernames.
+        - accounts: user saved accounts.
+        - deleted_accounts: accounts that have been deleted by user.
+        - unique_keys: Set of unique service names and usernames.
     """
-
     show_header(menu_titles["add"])
-    # Re-prompt user to re-enter service details when duplicate service names or usernames
-    # are detected during service registration
     chars_limit = 30
+    # Nested loop re-prompts user for username/service name when entries are exists in saved accounts
     while True:
-        # Re-prompt user for valid input when empty string is provided during input validation
         while True:
             service_name = input(messages["prompt"]["service_name"]).strip().lower()
             if not service_name:
                 show_message(messages["error"]["empty_service_field"])
-                continue  # Empty input detected
+                continue
             if len(service_name) > chars_limit:
                 show_message(messages["error"]["too_long_service_name"])
                 continue
             break
 
-        # Re-prompt user for valid input when empty string is provided during input validation
         while True:
             username = input(messages["prompt"]["username"]).strip().lower()
             if not username:
                 show_message(messages["error"]["empty_username_field"])
-                continue  # Empty input detected
+                continue
             if len(username) > chars_limit:
                 show_message(messages["error"]["too_long_username"])
                 continue
             break
 
         key = (service_name, username)
-        if key in unique_keys:  # check for duplicated data
+        if key in unique_keys:
             show_message(messages["error"]["exist"])
-            continue  # Duplicated data detected
+            continue
         break
 
-    # Re-prompt user for valid input when empty string is provided during input validation
     while True:
         password = input(messages["prompt"]["password"]).strip()
         if not password:
             show_message(messages["error"]["empty_password_field"])
-            continue  # Empty input detected
+            continue
         if len(password) > chars_limit:
             show_message(messages["error"]["too_long_password"])
             continue
