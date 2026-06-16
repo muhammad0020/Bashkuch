857cc574d776cdb659b5ea031cca5d7481889078
Muhammad Hosein Yeganeh <muhammad.hosein.yeganeh@gmail.com>
2026-06-16 11:37:09 +0330

    refactor(cleanup): remove unused modules

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,3 @@
 from .account_manager import AccountManager
-from .manage_accounts import manage_passwords
-from .delete_accounts import recycle_bin
 from .password_generator import generate_secure_password, length_validation
 from .storage import load_data
